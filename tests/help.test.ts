8d930402f7d434190b0a973024b5ea6fba60939b
Oskar Sommer <oskar.sommer@telekom.de>
2024-07-19 14:00:37 +0200

    start adding tests

---
+++ tests/help.test.ts
@@ -0,0 +1,46 @@
+import { expect, test } from "bun:test"
+
+const helpOutput = `
+Usage: localpen [options]
+
+Options:
+  -h, --help      Show this help message
+  -t, --template  Specify a template to use
+
+Deletion options, pick either or get prompted:
+  -k, --keep      Keep the project folder
+  -d, --delete    Delete the project folder
+
+Available templates:
+  - TypeScript/ts
+  - JavaScript/js
+  - Vite/v
+
+Vite templates:
+  - lit (-ts)
+  - preact (-ts)
+  - qwik (-ts)
+  - react (-ts)
+  - solid (-ts)
+  - svelte (-ts)
+  - vanilla (-ts)
+  - vue (-ts)
+`
+test("print help message", async () => {
+	const child = Bun.spawn(["bun", "run", "src/index.ts", "--help"])
+	const text = await new Response(child.stdout).text()
+
+	await child.exited
+
+	expect(text).toBe(helpOutput)
+})
+
+test("print help message on wrong template", async () => {
+	const child = Bun.spawn(["bun", "run", "src/index.ts", "--template", "foo"])
+	const text = await new Response(child.stdout).text()
+
+	await child.exited
+
+	expect(text.includes(helpOutput)).toBe(true)
+	expect(text.includes(" not found")).toBe(true)
+})
